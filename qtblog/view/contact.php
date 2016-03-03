47a96850b4370c0a541f5cc034fdc037661370fa
unknown <qt06.com@139.com>
2016-03-03 16:37:14 +0800

    somethings.

--- qtblog/view/contact.php
+++ qtblog/view/contact.php
@@ -1,7 +1,7 @@
 <? include 'header.php'; ?>
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('img/contact-bg.jpg')">
+    <header class="intro-header" style="background-image: url('<?=$conf['appurl']?>qtblog/view/img/contact-bg.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
